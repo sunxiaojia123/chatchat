bfab51ee5ffd7ec1a030737af7dee888ac2a2f5c
sunxiaojia <sunxiaojia2020@gmail.com>
2023-08-02 20:02:31 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI
+from fastapi import WebSocket, WebSocketDisconnect
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/health")
+def read_root():
+    return {"health": "ok"}
+
+
+from util.cache import ws_pool
+
+
+class WebsocketConnection(object):
+    def __init__(self, websocket: WebSocket, user_id):
+        self.websocket = websocket
+        ws_pool.add_connection(user_id, websocket)
+
+    async def get_msgs(self):
+        try:
+            while True:
+                msg_json = await self.websocket.receive_json()
+                yield msg_json
+        except Exception as e:
+            print(e)
+
+    async def msg_loop(self):
+        async for msg in self.get_msgs():
+            try:
+                await ws_pool.publish_msg(msg)
+            except Exception as e:
+                print(e)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket, user_id):
+    await websocket.accept()
+    conn = WebsocketConnection(websocket, user_id)
+    await conn.msg_loop()
+
+
+if __name__ == "__main__":
+    from gunicorn.app.base import BaseApplication
+
+
+    class GunicornApp(BaseApplication):
+        def __init__(self, app, options=None):
+            self.application = app
+            self.options = options or {}
+            super().__init__()
+
+        def load_config(self):
+            config = {key: value for key, value in self.options.items()
+                      if key in self.cfg.settings and value is not None}
+            for key, value in config.items():
+                self.cfg.set(key.lower(), value)
+
+        def load(self):
+            return self.application
+
+
+    options = {
+        'bind': '0.0.0.0:5000',
+        'workers': 3,
+        'threads': 32,
+        'worker_class': 'uvicorn.workers.UvicornWorker',
+        'timeout': 60 * 5,
+    }
+    GunicornApp(app, options).run()
---
+++ util/cache.py
@@ -0,0 +1,102 @@
+import json
+
+from redis import asyncio as aioredis
+import asyncio
+
+
+class AsyncRedis:
+    def __init__(self, host, port, password, db, pool_size: int = 10):
+        self.host = host
+        self.port = port
+        self.password = password
+        self.db = db
+        self.pool_size = pool_size
+        self.redis_store = None
+
+    async def initialize(self):
+        pool = aioredis.ConnectionPool(
+            host=self.host,
+            port=self.port,
+            db=self.db,
+            password=self.password,
+            max_connections=self.pool_size,
+            encoding="utf-8",
+            decode_responses=True,
+        )
+        self.redis_store = aioredis.Redis(connection_pool=pool)
+        return self
+
+    async def get_redis(self):
+        if self.redis_store is None:
+            await self.initialize()
+        else:
+            try:
+                await self.redis_store.ping()
+            except (aioredis.ConnectionError, aioredis.TimeoutError):
+                await self.initialize()
+        return self
+
+
+class SubChannel:
+    def __init__(self, redis: AsyncRedis, handleFunc, channel='message_channel_http'):
+        self.channel = self.get_channel(channel)
+        self.redis = redis
+        asyncio.get_event_loop().create_task(self._sub_listen(handleFunc))
+
+    def get_channel(self, channel):
+        return self.__class__.__name__ + ":" + channel
+
+    async def _pub_sub_init(self):
+        self.redis = await self.redis.get_redis()
+        self.pubsub = self.redis.redis_store.pubsub()
+        await self.pubsub.subscribe(self.channel)
+        return self.pubsub
+
+    async def _sub_listen(self, handleFunc):
+        print('sub listen')
+        await self._pub_sub_init()
+        while True:
+            try:
+                print('sub listen while')
+                _, _, data = await self.pubsub.parse_response()
+                data = json.loads(data)
+                print(data)
+                await handleFunc(data)
+            except (aioredis.ConnectionError, aioredis.TimeoutError):
+                await self._pub_sub_init()
+                await asyncio.sleep(1)
+            except TypeError:
+                pass
+            except Exception as e:
+                print(e)
+                await asyncio.sleep(1)
+        print('sub listen out')
+
+    async def publish_msg(self, data):
+        self.redis = await self.redis.get_redis()
+        await self.redis.redis_store.execute_command('publish', self.channel, json.dumps(data))
+
+    async def close(self):
+        self.pubsub.close()
+
+
+class WebsocketPool(SubChannel):
+    connections = {}
+
+    def add_connection(self, key, websocket):
+        self.connections[key] = websocket
+
+    def remove_connection(self, key):
+        del self.connections[key]
+
+    async def send_message(self, key, data):
+        await self.connections[key].send(data)
+
+    async def message_handle(self, data):
+        await self.send_message(data['key'], data['data'])
+
+    def __init__(self, redis: AsyncRedis):
+        super().__init__(redis=redis, handleFunc=self.message_handle)
+
+
+ws_pool = WebsocketPool(AsyncRedis('127.0.0.1', 6379, '', 0))
